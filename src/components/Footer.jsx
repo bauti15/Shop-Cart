89e3ba7c1a19bcd07afd10c1370795c900e667f2
bauti15 <bautistapiovani0@gmail.com>
2023-06-27 17:56:04 -0300

    primer commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import "./Footer.css";
+
+export function Footer() {
+  return (
+    <footer className="footer">
+      <h4>
+        Prueba tecnica de react * -<span>@bautistapiovani</span>{" "}
+      </h4>
+      <h5>Shopping Cart con useContext y UseReducer</h5>
+    </footer>
+  );
+}
